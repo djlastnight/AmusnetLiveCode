7b1a0c29054008941cab594674d2b3aa9071c822
Ivan Ivanov <i.ivanov@amusnet.com>
2023-05-03 08:47:36 +0300

    fix: add amusnet logo to the game

--- game/src/index.ts
+++ game/src/index.ts
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import Utils from "./core/Utils";
 import App from "./core/App";
 import SoundController from "./core/SoundController";
+import { Sprite, Texture } from "pixi.js";
 
 /**
  * Defines the game entry point.
@@ -23,6 +24,7 @@ async function entryPoint() {
  */
 async function loadAssets() {
     await SoundController.loadSounds([["click", "assets/audio/click.mp3"]]);
+    App.loader.add("logo", "assets/images/amusnet_logo.png");
 
     // Here is the place to load other assets.
 
@@ -51,11 +53,15 @@ function startGame() {
         gsap.fromTo(text, { angle: 0 }, { duration: 5.0, angle: 360 });
         await gsap.to(text.scale, { x: 2, y: 2, duration: 1.0, delay: 2.0 });
         await gsap.to(text.scale, { x: 1, y: 1, duration: 2.0 });
-        text.style.fill = "red";
-        text.anchor.set(0, 0);
-        gsap.to(text.position, { x: 0, y: 0, duration: 1.0 });
+        text.style.fill = "#a6ce39";
+        const logo = new Sprite(App.loader.resources["logo"].texture);
+        App.stage.addChild(logo);
+
+        text.anchor.set(0.5, 0);
+        gsap.to(text.position, { x: logo.width / 2, y: logo.height, duration: 1.0 });
         text.text = "Nice to meet you";
         SoundController.play("click");
+
     }, this);
 }
 
